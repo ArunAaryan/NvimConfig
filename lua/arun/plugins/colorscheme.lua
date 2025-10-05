3252e69cf8e0a0a5ad90cbcc131c1c28a548f331
ArunAaryan <rnyadav98@gmail.com>
2025-10-05 08:46:59 +0530

    feat (Colorscheme): - change color schema

--- lua/arun/plugins/colorscheme.lua
+++ lua/arun/plugins/colorscheme.lua
@@ -1,83 +1,124 @@
 return {
-  "rose-pine/neovim",
+  "navarasu/onedark.nvim",
   priority = 100,
   config = function()
-    require("rose-pine").setup({
-      variant = "auto", -- auto, main, moon, or dawn
-      dark_variant = "main", -- main, moon, or dawn
-      dim_inactive_windows = false,
-      extend_background_behind_borders = true,
+    -- Lua
+require('onedark').setup  {
+  -- Main options --
+  style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+  transparent = true,  -- Show/hide background
+  term_colors = true, -- Change terminal color as per the selected theme style
+  ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+  cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
-      enable = {
-        terminal = true,
-        legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-        migrations = true, -- Handle deprecated options automatically
-      },
+  -- toggle theme style ---
+  toggle_style_key = "<leader>ts", -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+  toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
 
-      styles = {
-        bold = true,
-        italic = true,
-        transparency = true,
-      },
+  -- Change code style ---
+  -- Options are italic, bold, underline, none
+  -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+  code_style = {
+      comments = 'italic',
+      keywords = 'none',
+      functions = 'none',
+      strings = 'none',
+      variables = 'none'
+  },
 
-      groups = {
-        border = "muted",
-        link = "iris",
-        panel = "surface",
+  -- Lualine options --
+  lualine = {
+      transparent = false, -- lualine center bar transparency
+  },
 
-        error = "love",
-        hint = "iris",
-        info = "foam",
-        note = "pine",
-        todo = "rose",
-        warn = "gold",
+  -- Custom Highlights --
+  colors = {}, -- Override default colors
+  highlights = {}, -- Override highlight groups
 
-        git_add = "foam",
-        git_change = "rose",
-        git_delete = "love",
-        git_dirty = "rose",
-        git_ignore = "muted",
-        git_merge = "iris",
-        git_rename = "pine",
-        git_stage = "iris",
-        git_text = "rose",
-        git_untracked = "subtle",
+  -- Plugins Config --
+  diagnostics = {
+      darker = true, -- darker colors for diagnostic
+      undercurl = true,   -- use undercurl instead of underline for diagnostics
+      background = true,    -- use background color for virtual text
+  },
+}
+    -- require("rose-pine").setup({
+    --   variant = "auto", -- auto, main, moon, or dawn
+    --   dark_variant = "main", -- main, moon, or dawn
+    --   dim_inactive_windows = false,
+    --   extend_background_behind_borders = true,
 
-        h1 = "iris",
-        h2 = "foam",
-        h3 = "rose",
-        h4 = "gold",
-        h5 = "pine",
-        h6 = "foam",
-      },
+    --   enable = {
+    --     terminal = true,
+    --     legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+    --     migrations = true, -- Handle deprecated options automatically
+    --   },
 
-      palette = {
-        -- Override the builtin palette per variant
-        -- moon = {
-        --     base = '#18191a',
-        --     overlay = '#363738',
-        -- },
-      },
+    --   styles = {
+    --     bold = true,
+    --     italic = true,
+    --     transparency = true,
+    --   },
 
-      highlight_groups = {
-        -- Comment = { fg = "foam" },
-        -- VertSplit = { fg = "muted", bg = "muted" },
-      },
+    --   groups = {
+    --     border = "muted",
+    --     link = "iris",
+    --     panel = "surface",
 
-      before_highlight = function(group, highlight, palette)
-        -- Disable all undercurls
-        -- if highlight.undercurl then
-        --     highlight.undercurl = false
-        -- end
-        --
-        -- Change palette colour
-        -- if highlight.fg == palette.pine then
-        --     highlight.fg = palette.foam
-        -- end
-      end,
-    })
+    --     error = "love",
+    --     hint = "iris",
+    --     info = "foam",
+    --     note = "pine",
+    --     todo = "rose",
+    --     warn = "gold",
 
-    vim.cmd("colorscheme rose-pine-moon")
+    --     git_add = "foam",
+    --     git_change = "rose",
+    --     git_delete = "love",
+    --     git_dirty = "rose",
+    --     git_ignore = "muted",
+    --     git_merge = "iris",
+    --     git_rename = "pine",
+    --     git_stage = "iris",
+    --     git_text = "rose",
+    --     git_untracked = "subtle",
+
+    --     h1 = "iris",
+    --     h2 = "foam",
+    --     h3 = "rose",
+    --     h4 = "gold",
+    --     h5 = "pine",
+    --     h6 = "foam",
+    --   },
+
+    --   palette = {
+    --     -- Override the builtin palette per variant
+    --     -- moon = {
+    --     --     base = '#18191a',
+    --     --     overlay = '#363738',
+    --     -- },
+    --   },
+
+    --   highlight_groups = {
+    --     -- Comment = { fg = "foam" },
+    --     -- VertSplit = { fg = "muted", bg = "muted" },
+    --   },
+
+    --   before_highlight = function(group, highlight, palette)
+    --     -- Disable all undercurls
+    --     -- if highlight.undercurl then
+    --     --     highlight.undercurl = false
+    --     -- end
+    --     --
+    --     -- Change palette colour
+    --     -- if highlight.fg == palette.pine then
+    --     --     highlight.fg = palette.foam
+    --     -- end
+    --   end,
+    -- })
+
+    -- vim.cmd("colorscheme rose-pine-moon")
+    vim.cmd("colorscheme onedark")
   end,
   -- vim.cmd("colorscheme rose-pine-main")
   -- vim.cmd("colorscheme rose-pine-moon")
