c4a9c7aa815eec531983953d6b76f1f06b5ca330
ArunAaryan <rnyadav98@gmail.com>
2026-04-26 22:24:59 +0530

    fix (Remove fmt formatter): - remove breaking fmt formatter - remove default formatters

--- lua/arun/plugins/lsp/mason.lua
+++ lua/arun/plugins/lsp/mason.lua
@@ -47,13 +47,10 @@ return {
     mason_tool_installer.setup({
       ensure_installed = {
         "prettier", -- prettier formatter
-        "stylua", -- lua formatter
         "isort", -- python formatter
         "black", -- python formatter
         "pylint", -- python linter
         "eslint_d", -- js linter
-        "rustfmt", -- rust formatter
-        -- "rust-analyzer", -- rust language server
       },
     })
   end,
